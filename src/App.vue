7f1a1f14fc50e9847ed50100a7fa6f113f905d20
comphca <comphca@gmail.com>
2022-01-25 23:40:14 +0800

    登录界面

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "assets/css/main.css";
+@import "assets/css/color-dark.css"; /*深色主题*/
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +15,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
